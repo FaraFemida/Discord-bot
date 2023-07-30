33933740e2fdc2f25a748fff99d4736d62565439
FaraFemida <farafemida@gmail.com>
2023-07-30 08:05:57 +0600

    понаделал херни бесполезной, позже исправлю

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
-const { Client, GatewayIntentBits, EmbedBuilder } = require('discord.js');
+const { Client, GatewayIntentBits, EmbedBuilder, Partials } = require('discord.js');
 const { format } = require('date-fns');
 const { ru } = require('date-fns/locale');
 const config = require('./config.json');
 
 const client = new Client({
+  partials: [Partials.MESSAGE, Partials.CHANNEL, Partials.REACTION],
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildVoiceStates, // Добавляем право GUILD_VOICE_STATES
+    GatewayIntentBits.GuildMembers, // Добавляем право GUILD_MEMBERS
   ],
 });
+
 
 const SLASH_COMMANDS = [
   // Добавьте сюда имена всех слеш-команд, которые должны быть зарегистрированы на сервере
@@ -44,7 +49,7 @@ client.once('ready', async () => {
 client.on('interactionCreate', async (interaction) => {
   if (!interaction.isCommand()) return;
 
-  const { commandName, guild } = interaction; // Переместили определение переменной guild сюда
+  const { commandName } = interaction; // Переместили определение переменной guild сюда
 
   if (commandName === 'serverinfo') {
     const guild = interaction.guild;
@@ -61,6 +66,13 @@ client.on('interactionCreate', async (interaction) => {
     const offlineMembers = memberCount - activeMembers;
 
     const voiceChannels = guild.channels.cache.filter((channel) => channel.type === 'GUILD_VOICE'); // Добавлено: Получаем список голосовых каналов сервера
+
+           // Подсчитываем общее количество участников в голосовых каналах
+           let voiceChannelMembersCount = 0;
+           voiceChannels.forEach((voiceChannel) => {
+             voiceChannelMembersCount += voiceChannel.members.size;
+           });
+           
     const voiceChannelMembers = voiceChannels.reduce((totalMembers, voiceChannel) => totalMembers + voiceChannel.members.size, 0); // Добавлено: Считаем количество участников в голосовых каналах
     const createdAt = guild.createdAt;
     const formattedDate = format(createdAt, 'PPP', { locale: ru }); // Форматируем дату в виде 'день месяц год' (например, '28 июля 2023')
@@ -73,8 +85,6 @@ client.on('interactionCreate', async (interaction) => {
          return;
        }
 
-    const serverOwnerName = serverOwner.username;
-    const serverOwnerDiscriminator = serverOwner.discriminator;
     const serverOwnerId = serverOwner.id;
 
     const embed = new EmbedBuilder()
@@ -82,12 +92,12 @@ client.on('interactionCreate', async (interaction) => {
       .setImage('https://i.pinimg.com/originals/bc/53/d1/bc53d1661adc7443e7be761f6f6ab961.gif') // Установили указанную ссылку как большую картинку
       .setTitle('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀**Информация о сервере**')
       .addFields(
-        { name: '\u200B', value: '<:serverinfo:1134989793294549103> **Дата создания сервера: **' + formattedDate },
-        { name: '\u200B', value: '<:memberblue:1134987992554025101> **Всего участников: **' + memberCount.toString() },
-        { name: '\u200B', value: '<:membergreen:1134984700230914118> **Онлайн участники: **' + activeMembers.toString() },
-        { name: '\u200B', value: '<:memberred:1134984695604588605> **Оффлайн участники: **' + offlineMembers.toString() },
-        { name: '\u200B', value: '<:voicebadge:1134988862536560721> **Участники в голосовых каналах: **' + voiceChannelMembers.toString() },
-        { name: '\u200B', value: '<:ownercrown:1134984698393804920> **Создатель сервера: **' + '<@'+serverOwnerId+'>' },
+        { name: '\u200B', value: '<:serverinfo:1134989793294549103> **Дата создания сервера: **' + formattedDate, inline: true },
+        { name: '\u200B', value: '<:memberblue:1134987992554025101> **Всего участников: **' + memberCount.toString(), inline: true },
+        { name: '\u200B', value: '<:membergreen:1134984700230914118> **Онлайн участники: **' + activeMembers.toString(), inline: true },
+        { name: '\u200B', value: '<:memberred:1134984695604588605> **Оффлайн участники: **' + offlineMembers.toString(), inline: true },
+        { name: '\u200B', value: '<:voicebadge:1134988862536560721> **Участники в голосовых каналах: **' + voiceChannelMembers.toString(), inline: true },
+        { name: '\u200B', value: '<:ownercrown:1134984698393804920> **Создатель сервера: **' + '<@'+serverOwnerId+'>', inline: true },
       )
       .setTimestamp() // Добавлено: устанавливаем текущую дату/время как timestamp эмбеда,
       .setFooter({ text: 'De/Generation', iconURL: 'https://i.pinimg.com/564x/88/b2/f7/88b2f7aae4fd60ed92f53f47e5c69daa.jpg' });
